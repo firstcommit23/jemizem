03efdca8e68f461c45391e8a752d905d3c308904
jm131313 <jm131313.kim@gmail.com>
2020-05-06 22:36:01 +0900

    index 테스트

--- src/main/java/com/jemizem/blog/springboot/web/IndexController.java
+++ src/main/java/com/jemizem/blog/springboot/web/IndexController.java
@@ -1,14 +1,12 @@
 package com.jemizem.blog.springboot.web;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
-    @GetMapping("/gg")
+    @GetMapping("/")
     public String index() {
 
         return "index";
