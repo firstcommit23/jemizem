48fed9ec220bed756fb2af73326c05afe2093cd9
jm131313 <jm131313.kim@gmail.com>
2020-05-13 23:53:16 +0900

    달력 만드는 중

--- src/main/java/com/jemizem/blog/springboot/web/IndexController.java
+++ src/main/java/com/jemizem/blog/springboot/web/IndexController.java
@@ -1,10 +1,24 @@
 package com.jemizem.blog.springboot.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class IndexController {
+    Calendar cal = Calendar.getInstance();
+
+    private int startDay;   // 월 시작 요일
+    private int lastDay;    // 월 마지막 날짜
+    private int inputDate=1;  // 입력 날짜
 
     @GetMapping("/")
     public String index() {
@@ -14,8 +28,24 @@ public class IndexController {
     }
 
     @GetMapping("/calendar")
-    public String viewCalendar() {
+    public String viewCalendar(Model model)  {
+        ArrayList<Map> ret = new ArrayList<Map>();
 
+
+        startDay = cal.get(Calendar.DAY_OF_WEEK); // 월 시작 요일
+
+        lastDay = cal.getActualMaximum(Calendar.DATE); // 월 마지막 날짜
+
+        
+        for (int i=0; i<35; i++) {
+            Map<String, String> mapCal = new HashMap<>();
+            mapCal.put("day",String.valueOf(i+1));
+            mapCal.put("br",((i>0 && (i+1)%7==0) )?null:"True");
+
+            ret.add(i, mapCal);
+        }
+        model.addAttribute("calendar", ret);
+        
         return "calendar";
 
     }
